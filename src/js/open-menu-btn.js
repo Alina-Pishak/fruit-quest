69aaad1827a7d24628a42fc40a362be4ae59c45d
Alina-Pishak <alinapishak@icloud.com>
2024-06-25 13:42:38 +0300

    add smooth scroll and close menu by click

--- src/js/open-menu-btn.js
+++ src/js/open-menu-btn.js
@@ -1,6 +1,7 @@
 const openMenuBtn = document.querySelector('#open-menu-btn');
 const closeMenuBtn = document.querySelector('#close-menu-btn');
 const mobileMenu = document.querySelector('#mobile-menu');
+const mobileMenuLink = document.querySelector('#mobile-menu-link');
 const body = document.querySelector('body');
 
 const toggleMobileMenu = () => {
@@ -12,3 +13,5 @@ const toggleMobileMenu = () => {
 openMenuBtn.addEventListener('click', toggleMobileMenu);
 
 closeMenuBtn.addEventListener('click', toggleMobileMenu);
+
+mobileMenuLink.addEventListener('click', toggleMobileMenu);
