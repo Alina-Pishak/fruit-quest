77162a883307ca4367c2b025c36a1a1950a9990f
Alina-Pishak <alinapishak@icloud.com>
2024-06-25 12:53:26 +0300

    add js to mob menu

--- src/js/open-menu-btn.js
+++ src/js/open-menu-btn.js
@@ -1,3 +1,14 @@
 const openMenuBtn = document.querySelector('#open-menu-btn');
-const mobileMenu = '';
-openMenuBtn.addEventListener('click', () => {});
+const closeMenuBtn = document.querySelector('#close-menu-btn');
+const mobileMenu = document.querySelector('#mobile-menu');
+const body = document.querySelector('body');
+
+const toggleMobileMenu = () => {
+  mobileMenu.classList.toggle('hidden');
+  body.classList.toggle('overflow-hidden');
+  console.log('click');
+};
+
+openMenuBtn.addEventListener('click', toggleMobileMenu);
+
+closeMenuBtn.addEventListener('click', toggleMobileMenu);
